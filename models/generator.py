b295ea1f8dabd62644ab9b7e1e0167958702fd27
Matheus Pedroni <pnmatheus@protonmail.com>
2022-06-05 21:33:02 -0300

    conditional generator model

--- models/generator.py
+++ models/generator.py
@@ -3,6 +3,7 @@ Generator models for GANs.
 """
 
 from typing import List
+
 import torch
 from torch import nn
 
@@ -44,3 +45,49 @@ class Generator(nn.Module):
         Generates an image from a noise vector.
         """
         return self.conv_modules(z.view(z.size(0), z.size(1), 1, 1))
+
+
+class CondGenerator(nn.Module):
+    """ Conditional generator for CondGAN model. """
+
+    def __init__(
+            self, input_dim: int, embedding_size: int, embedding_dim: int,
+            hidden_dims: List[int] = [512, 256, 128, 64]):
+        super().__init__()
+        conv_modules = []
+        hidden_dims = [input_dim + embedding_dim] + hidden_dims + [3] # 3 channels output
+        conv_modules.append(nn.Sequential(
+            nn.ConvTranspose2d(hidden_dims[0], hidden_dims[1], kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(hidden_dims[1]),
+            nn.ReLU(inplace=True)
+        ))
+        for i in range(1, len(hidden_dims) - 2):
+            conv_modules.append(nn.Sequential(
+                nn.ConvTranspose2d(hidden_dims[i], hidden_dims[i+1], kernel_size=4, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(hidden_dims[i+1]),
+                nn.ReLU(inplace=True)
+            ))
+        conv_modules.append(nn.Sequential(
+            nn.ConvTranspose2d(hidden_dims[-2], hidden_dims[-1], kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+        ))
+        self.conv_modules = nn.Sequential(*conv_modules)
+        self.embedding = nn.Embedding(embedding_size, embedding_dim)
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, 0.0, 0.02)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, 1.0, 0.02)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, z: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+        """
+        Generates an image from a noise vector and category labels.
+        """
+        embedding = self.embedding(labels)
+        z = torch.cat([z, embedding], dim=1)
+        z = z.view(z.size(0), z.size(1), 1, 1)
+        return self.conv_modules(z)
