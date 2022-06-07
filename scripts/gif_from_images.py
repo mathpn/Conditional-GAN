869bcf391aca284f5bcfb9d34b1033b39f1834d6
Matheus Pedroni <pnmatheus@protonmail.com>
2022-06-06 21:04:49 -0300

    center crop GIFs

--- scripts/gif_from_images.py
+++ scripts/gif_from_images.py
@@ -18,11 +18,14 @@ def main():
     imgs = sorted(imgs, key=lambda x: int(x.split('.')[0].split('_')[-1]))
     for i in imgs:
         new_frame = Image.open(i)
+        new_frame = new_frame.crop((90, 90, new_frame.size[0] - 75, new_frame.size[1] - 75))
+        new_frame = new_frame.resize((512, 512))
         frames.append(new_frame)
     
     frames[0].save(f'{prefix}.gif', format='GIF',
                 append_images=frames[1:],
                 save_all=True,
+                optimize=True,
                 duration=300, loop=0)
     print(f'{prefix}.gif saved.')
 
